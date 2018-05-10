631e49088a0e25aeaed653e1a9f32d1bb846f88d
Takashi Kawasaki <espresso3389@gmail.com>
2018-05-10 18:46:10 +0900

    FIXED: #3.

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,26 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
+import * as childProcess from 'child_process';
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-    let terminalStacks = new Map<string, vscode.Terminal[]>();
+interface VSWhere {
+    installationPath: string;
+    displayName: string;
+    catalog: {
+        productDisplayVersion: string;
+        productLineVersion: string;
+    }
+}
 
-    let shell = (vscode.workspace.getConfiguration().get('terminal.integrated.shell.windows') as string).toLowerCase();
-    let system32 = 'system32'; //shell.indexOf("sysnative") > 0 ? 'sysnative' : 'system32';
-    let isPowershell = shell.indexOf("powershell.exe") > 0;
-    let isCmdExe = shell.indexOf("cmd.exe") > 0;
-    let isWslBash = shell.indexOf("bash.exe") > 0;
+export function activate(context: vscode.ExtensionContext) {
+    const terminalStacks = new Map<string, vscode.Terminal[]>();
+
+    const shell = (vscode.workspace.getConfiguration().get('terminal.integrated.shell.windows') as string).toLowerCase();
+    const system32 = 'system32'; //shell.indexOf("sysnative") > 0 ? 'sysnative' : 'system32';
+    const isPowershell = shell.indexOf("powershell.exe") > 0;
+    const isCmdExe = shell.indexOf("cmd.exe") > 0;
+    const isWslBash = shell.indexOf("bash.exe") > 0;
     if (!isPowershell && !isCmdExe && !isWslBash)
     {
         vscode.window.showErrorMessage('Could not determine shell type. It should be either cmd.exe or powershell.exe in system32 or sysnative directory.');
@@ -21,18 +31,12 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('terminalVcVers.createBashShell', () => {
         showTerminal('c:\\Windows\\' + system32 + '\\bash.exe', 'bash');
     }));
-    context.subscriptions.push(vscode.commands.registerCommand('terminalVcVers.createVsCmdPrompt', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('terminalVcVers.createVsCmdPrompt', async() => {
         let commands = new Map<string,string>();
-        for (let vs of vsEditions()) {
-            let vcvarsall = vs[0];
-            let vsname = vs[1];
-            if (fs.existsSync(vcvarsall)) {
-                for (let arch of ['x86', 'amd64'])
-                    commands.set(`${vsname} (${arch})`, `"${vcvarsall}" ${arch}`);
-            }
+        for (let vs of await getVsEditionsAsync()) {
+            commands.set(vs[1], vs[0]);
         }
-        if (commands.size == 0)
-        {
+        if (commands.size == 0) {
             vscode.window.showErrorMessage('No Visual Studio installation found.');
             return;
         }
@@ -42,28 +46,62 @@ export function activate(context: vscode.ExtensionContext) {
             .then(n => showCmd64(commands.get(n), n));
     }));
 
-    function *vsEditions(): Iterable<[string, string]> {
-        let vsvers = {'90': '2008', '100': '2010', '110': '2012', '120': '2013', '140': '2015'};
-        for (let vsver in vsvers) {
-            let vstooldir = process.env['VS' + vsver + 'COMNTOOLS'];
-            let vsname = 'VS' + vsvers[vsver];
+    async function getVsEditionsAsync(): Promise<[string, string][]> {
+        const ret: [string, string][] = [];
+        const vsvers = {'90': '2008', '100': '2010', '110': '2012', '120': '2013', '140': '2015'};
+        for (const vsver in vsvers) {
+            const vstooldir = process.env['VS' + vsver + 'COMNTOOLS'];
+            const vsname = 'VS' + vsvers[vsver];
             if (vstooldir) {
-                let vcvarsall = path.join(vstooldir, '..\\..\\VC\\vcvarsall.bat');
-                yield [vcvarsall, 'VS' + vsvers[vsver]];
+                const vcvarsall = path.join(vstooldir, '..\\..\\VC\\vcvarsall.bat');
+                if (fs.existsSync(vcvarsall)) {
+                    for (let arch of ['x86', 'amd64']) {
+                        const name = `VS ${vsvers[vsver]} (${arch})`;
+                        const cmd = `"${vcvarsall}" ${arch}`;
+                        ret.push([cmd, name]);
+                    }
+                }
             }
         }
 
-        // FIXME: hardcoded path for new Visual Studio versions/editions.
-        let vsvers2 = ['2017', 'Preview'];
-        let editions = ['Community', 'Professional', 'Enterprise'];
-        for (let i = 0; i < vsvers2.length; i++) {
-            let v = vsvers2[i];
-            let vsdirRoot = path.join(process.env['ProgramFiles(x86)'], 'Microsoft Visual Studio', v);
-            for (let j = 0; j < editions.length; j++) {
-                let ed = editions[j];
-                yield [path.join(vsdirRoot, ed, 'VC\\Auxiliary\\Build\\vcvarsall.bat'), 'VS' + v + ' ' + ed];
+        for (const vsi of await enumAllVsInstallationsAsync()) {
+            const vcvarsall = path.join(vsi.installationPath, 'VC\\Auxiliary\\Build\\vcvarsall.bat');
+            const dname = vsi.displayName.replace('Visual Studio', 'VS');
+            // FIXME: we need arm or arm64 here. See #4
+            for (let arch of ['x86', 'amd64']) {
+                const cmd = `"${vcvarsall}" ${arch}`;
+                ret.push([cmd, `${dname} ${vsi.catalog.productDisplayVersion} (${arch})`]);
             }
         }
+        return ret;
+    }
+
+    async function enumAllVsInstallationsAsync() {
+        // https://github.com/Microsoft/vswhere
+        const vswhereExe = path.join(process.env['ProgramFiles(x86)'], 'Microsoft Visual Studio\\Installer\\vswhere.exe');
+        const buf = await getRedirectedDataAsync(vswhereExe, '-all', '-prerelease', '-format', 'json');
+        return JSON.parse(buf.toString()) as VSWhere[];
+    }
+    
+    async function getRedirectedDataAsync(command: string, ...args: string[]) {
+        return new Promise<Buffer>((resolve, reject) => {
+            try {
+                const proc = childProcess.spawn(command, args);
+                let data = Buffer.alloc(0);
+                proc.stdout.on('data', chunk => {
+                    data = Buffer.concat([data, chunk as Buffer]);
+                });
+                proc.on('close', () => {
+                    try {
+                        resolve(data);
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                });
+            } catch (ex) {
+                reject(ex);
+            }
+        });
     }
 
     function showTerminal(cmd: string, caption: string) {
